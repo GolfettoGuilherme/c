acc756dbb126b558c72bb0ef290fb27aa8af54a4
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2016-08-22 19:11:43 -0300

    primeiras alterações

--- aula002/main.c
+++ aula002/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+struct aluno{
+    char curso[60];
+    int semestre;
+};
+typedef struct aluno reg;
+
+int main(){
+    reg *a;
+    a = (reg*) malloc(sizeof(reg));
+    printf("Curso:");
+    gets(a->curso);
+    printf("Semestre:");
+    scanf("%d",&a->semestre);
+    printf("Curso %s, semestre %d",a->curso,a->semestre);
     return 0;
 }
