92e18cb331a31ac384fc9ff0941ccbf765427309
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2016-08-22 21:18:54 -0300

    Fim de aula

--- aula002/main.c
+++ aula002/main.c
@@ -2,18 +2,46 @@
 #include <stdlib.h>
 
 struct aluno{
-    char curso[60];
-    int semestre;
+   char curso[60];
+   int semestre;
+   struct aluno *prox;
 };
 typedef struct aluno reg;
 
+void criaCelula();
+
+reg *inicio=NULL,*atual,*aux;
+
+void criaCelula(){
+    _flushall();
+   if(inicio==NULL){
+      inicio=(reg *)malloc(sizeof(reg));
+      inicio->prox=NULL;
+      aux=inicio;
+      printf("Curso:");
+      gets(inicio->curso);
+      printf("Semestre:");
+      scanf("%d",&inicio->semestre);
+   } else{
+       atual=(reg*)malloc(sizeof(reg));
+       atual->prox=NULL;
+       aux->prox=atual;
+       aux=atual;
+       printf("Curso:");
+       gets(atual->curso);
+       printf("Semestre:");
+       scanf("%d",&atual->semestre);
+   }
+}
+
+
 int main(){
-    reg *a;
-    a = (reg*) malloc(sizeof(reg));
-    printf("Curso:");
-    gets(a->curso);
-    printf("Semestre:");
-    scanf("%d",&a->semestre);
-    printf("Curso %s, semestre %d",a->curso,a->semestre);
+    char resp = 's';
+    while(resp == 's'){
+        criaCelula();
+        _flushall();
+        printf("Deseja continuar (s/n)");
+        scanf("%c", &resp);
+    }
     return 0;
 }
